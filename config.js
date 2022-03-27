55571e4fc2475eb5d9b4cc1539d056da1263766d
magga <adb.dao@gmail.com>
2022-03-27 20:01:40 +0800

    add j13.cor

--- config.js
+++ config.js
@@ -1,17 +1,36 @@
-﻿	window.accelon2017={
-		corpora:["vinaya2","samgha_picture","v4search","yugasidi","ajm","zhimin","taisho","nanchuan","yinshun","taixu","mpps","manual"],
-		dictionaries:[
-	{name:"一行",url:"http://buddhaspace.org/dict/index.php?keyword=$$"},
-	{name:"萌典",url:"https://www.moedict.tw/$$"},
-	{name:"康熙",url:"http://kangxi.adcs.org.tw/kangxizidian/#$$",max:1},
-	{name:"維基",url:"http://zh.wikipedia.org/w/index.php?title=$$&variant=zh-tw"},
-	{name:"法鼓",url:"http://isearch.dila.edu.tw/search.html?q=$$"},
-	{name:"阿含",url:"http://agama.buddhason.org/ad/adSearch.php?keyword=$$"},
-	{name:"CB漢字",url:"http://dict.cbeta.org/word/search.php?op=search&scope=3&word=$$",max:1},
-	{name:"IDS漢字",url:"http://www.chise.org/ids-find?components=$$",max:1},
-	{name:"英漢",url:"http://cdict.net/?q=$$"},
-	{name:"Bing",url:"https://www.bing.com/search?q=$$&PC=U316&FORM=CHROMN"},
-	{name:"Youtube",url:"https://www.youtube.com/results?search_query=$$&page={startPage?}&utm_source=opensearch"}
+﻿window.accelon2017 = {
+	corpora: [
+		"vinaya2",
+		"sila",
+		"samgha_picture",
+		"v4search",
+		"yugasidi",
+		"ajm",
+		"zhimin",
+		"ts_lin",
+		"buddhist_dictionary",
+		"panna2011",
+		"kt",
+		"taisho",
+		"nanchuan",
+		"yinshun",
+		"taixu",
+		"mpps",
+		"manual",
+		"j13"
+	],
+	dictionaries: [
+		{ name: "一行", url: "http://buddhaspace.org/dict/index.php?keyword=$$" },
+		{ name: "萌典", url: "https://www.moedict.tw/$$" },
+		{ name: "康熙", url: "http://kangxi.adcs.org.tw/kangxizidian/#$$", max: 1 },
+		{ name: "維基", url: "http://zh.wikipedia.org/w/index.php?title=$$&variant=zh-tw" },
+		{ name: "法鼓", url: "http://isearch.dila.edu.tw/search.html?q=$$" },
+		{ name: "阿含", url: "http://agama.buddhason.org/ad/adSearch.php?keyword=$$" },
+		{ name: "CB漢字", url: "http://dict.cbeta.org/word/search.php?op=search&scope=3&word=$$", max: 1 },
+		{ name: "IDS漢字", url: "http://www.chise.org/ids-find?components=$$", max: 1 },
+		{ name: "英漢", url: "http://cdict.net/?q=$$" },
+		{ name: "Bing", url: "https://www.bing.com/search?q=$$&PC=U316&FORM=CHROMN" },
+		{ name: "YouTube", url: "https://www.youtube.com/results?search_query=$$&page={startPage?}&utm_source=opensearch" }
 
-]
-	}
+	]
+}
